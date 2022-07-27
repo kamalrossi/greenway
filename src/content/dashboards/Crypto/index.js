302b21335cb582870eb76fc6d8083c9ad67d811e
Rossi Kamal <rossikamal@gmail.com>
2022-07-28 02:03:46 +0600

    Update index.js

--- src/content/dashboards/Crypto/index.js
+++ src/content/dashboards/Crypto/index.js
@@ -13,7 +13,7 @@ function DashboardCrypto() {
   return (
     <>
       <Helmet>
-        <title>Crypto Dashboard</title>
+        <title>Greenway Dashboard</title>
       </Helmet>
       <PageTitleWrapper>
         <PageHeader />
